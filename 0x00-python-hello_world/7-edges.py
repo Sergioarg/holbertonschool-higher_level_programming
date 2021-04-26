e495e632dbdbd01acf7a2ba8efa4dd0d0c3e89c3
Sergioarg <sergio_ramos53@yahoo.com>
2021-04-26 18:30:42 -0500

    Add 7-edges.py

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word
+word_last_2 = word[7:]
+middle_word = word[1:8]
+print("First 3 letters: {:.3}".format(word_first_3))
+print("Last 2 letters: {:s}".format(word_last_2))
+print("Middle word: {:s}".format(middle_word))
