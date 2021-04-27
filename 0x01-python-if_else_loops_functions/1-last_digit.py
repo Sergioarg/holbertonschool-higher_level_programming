c0b973eabc90d67b6e6abc5bfb4d2bc8b70c12a9
Sergioarg <sergio_ramos53@yahoo.com>
2021-04-27 17:33:26 -0500

    (Refactor) and add ternary condition

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-p_last = (number % 10)
-n_last = (number % -10)
 msg = "Last digit of"
-# POSITIVE NUMBERS
-if (number > 0):
-    if (p_last == 0):
-        print(msg, number, "is", p_last, "and is 0")
-    elif (p_last > 5):
-        print(msg, number, "is", p_last, "and is greater than 5")
-    else:
-        print(msg, number, "is", p_last, "and is less than 6 and not 0")
-# NEGATIVE NUMBERS
-if (number < 0):
-    if (n_last == 0):
-        print(msg, number, "is", p_last, "and is 0")
-    elif (n_last > 5):
-        print(msg, number, "is", n_last, "and is greater than 5")
-    else:
-        print(msg, number, "is", n_last, "and is less than 6 and not 0")
+last = number % 10 if (number > 0) else (number % -10)
+
+if (last == 0):
+    print(msg, number, "is", last, "and is 0")
+elif (last > 5):
+    print(msg, number, "is", last, "and is greater than 5")
+else:
+    print(msg, number, "is", last, "and is less than 6 and not 0")
