457f7a021d42e9911747a018c2cbc685be670669
Sergioarg <sergio_ramos53@yahoo.com>
2021-04-27 19:54:10 -0500

    Change identation to spaces

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for number in range(0, 99):
-	print('{:n}'.format(number), '=', hex(number))
+    print('{:n} ='.format(number), hex(number))
+    # print('{:d} = 0x{:x}'.format(number, number))
