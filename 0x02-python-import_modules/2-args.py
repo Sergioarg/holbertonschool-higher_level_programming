a2b3551d6b702f80257a5ff0724b7b849baaa089
Sergioarg <sergio_ramos53@yahoo.com>
2021-04-28 22:51:20 -0500

    (Add) code

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+count = len(sys.argv)
+if (count == 1):
+    print("0 arguments.")
+elif (count == 2):
+    print("1 argument:")
+else:
+    print('{:d} arguments:'.format((count - 1)))
+for arguments in range(1, count):
+    if (count > 1):
+        print('{:d}: {:s}'.format(arguments, sys.argv[arguments]))
