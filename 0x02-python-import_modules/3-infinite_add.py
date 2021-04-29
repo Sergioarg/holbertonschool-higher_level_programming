e14470b41d704c2f5f42c17c7b8a5b818c134eb7
Sergioarg <sergio_ramos53@yahoo.com>
2021-04-29 00:22:01 -0500

    (Add) code

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    add = 0
+    count = len(sys.argv)
+    if (count == 1):
+        print("0")
+    for number in range(1, count):
+        add += int(sys.argv[number])
+    print('Add: {:d}'.format(add))
