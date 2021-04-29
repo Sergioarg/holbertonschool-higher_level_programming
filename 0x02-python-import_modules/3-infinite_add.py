58bdc46ac6b72eb8ea81c8ae926b2530283e1d0c
Sergioarg <sergio_ramos53@yahoo.com>
2021-04-29 00:28:47 -0500

    (Fix) edge case

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,8 +3,6 @@ if __name__ == "__main__":
     import sys
     add = 0
     count = len(sys.argv)
-    if (count == 1):
-        print("0")
     for number in range(1, count):
         add += int(sys.argv[number])
     print('{:d}'.format(add))
