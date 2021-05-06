8030c93d5ae436563af6ee225be0b8411a430fae
Sergioarg <sergio_ramos53@yahoo.com>
2021-05-06 00:05:31 -0500

    :tada:(Add) 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if not my_list:
-        return
-    new_list = []
+
+    new_list = my_list[:]
     if (idx < 0 or idx >= len(my_list)):
         return (new_list)
+    new_list[idx] = element
 
-    for i in range(0, len(my_list)):
-        if (i == idx):
-            new_list.append(element)
-        else:
-            new_list.append(my_list[i])
     return (new_list)
