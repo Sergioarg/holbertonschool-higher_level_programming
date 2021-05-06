cb9bbf8bfb8df3e913fd59a18897cba0147436ac
Sergioarg <sergio_ramos53@yahoo.com>
2021-05-05 20:03:48 -0500

    (Add) 4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = []
+    if (idx < 0):
+        return (new_list)
+
+    if (idx >= len(my_list)):
+        return (new_list)
+
+    for i in range(len(my_list)):
+        if (i == idx):
+            new_list.append(element)
+        else:
+            new_list.append(my_list[i])
+    return (new_list)
