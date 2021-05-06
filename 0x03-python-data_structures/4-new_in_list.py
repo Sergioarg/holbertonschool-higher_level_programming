b2746b9a57f4e3e3942a6a3e181f394a8f0c5348
Sergioarg <sergio_ramos53@yahoo.com>
2021-05-05 21:04:41 -0500

    (Fix) Changes conditionals

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = []
-    if my_list:
-        if (idx < 0 or idx >= len(my_list)):
-            return (new_list)
-
-        for i in range(0, len(my_list)):
-            if (idx != i):
-                new_list.append(my_list[i])
-            else:
-                new_list.append(element)
-    else:
+    if not my_list:
         return
+    new_list = []
+    if (idx < 0 or idx >= len(my_list)):
+        return (new_list)
+
+    for i in range(0, len(my_list)):
+        if (i == idx):
+            new_list.append(element)
+        else:
+            new_list.append(my_list[i])
     return (new_list)
