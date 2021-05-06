53bd04b4370f26a010c176741199252037486db9
Sergioarg <sergio_ramos53@yahoo.com>
2021-05-05 22:42:40 -0500

    :pencil:(Fix) format pep8

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
-            print(row[i], end=' ' if not (i is len(row) - 1) else '')
+            print('{:d}'.format(row[i]),
+                  end=' ' if not (i is len(row) - 1) else '')
         print()
