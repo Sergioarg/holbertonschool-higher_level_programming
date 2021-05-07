6a3b3cbbac30dc9a90a0674d16fa4dcdd81ff0cb
Sergioarg <sergio_ramos53@yahoo.com>
2021-05-07 12:30:55 -0500

    (Fix) reduce numbers of lines

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    new_matrix = map(lambda row: list(map(lambda n: n ** 2, row)), matrix)
-    new_list = list(new_matrix)
-    return (new_list)
+    return (list(map(lambda row: list(map(lambda n: n ** 2, row)), matrix)))
