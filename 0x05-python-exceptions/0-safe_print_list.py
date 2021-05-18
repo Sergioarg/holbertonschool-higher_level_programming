41cb9d4a273666df7bbe1510da5c5d7f615dc496
Sergioarg <sergio_ramos53@yahoo.com>
2021-05-17 22:33:38 -0500

    Counter out of try

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/pyhon3
 def safe_print_list(my_list=[], x=0):
+    counter = 0
     try:
-        lenght = 0
-        for i in range(x):
+        for i in range(0, x):
             print('{}'.format(my_list[i]), end="")
-            lenght = lenght + 1
+            counter += 1
         print("")
     except IndexError:
         print("")
-    return lenght
-
+    return counter
