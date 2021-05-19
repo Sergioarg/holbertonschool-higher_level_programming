2ca63a8d97fe910f6da447fdfba09e2686020351
Sergioarg <sergio_ramos53@yahoo.com>
2021-05-18 19:17:55 -0500

    (Add) 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+""" Create the Square class """
+
+
+from typing import Sized
+
+
+class Square:
+
+    """ Valid that the size is number."""
+
+    def __init__(self, size=0):
+        if(type(size) != int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = int(size)
+
+    """ Get the area of the square."""
+    def area(self):
+        number = self.__size
+        return(number * number)
