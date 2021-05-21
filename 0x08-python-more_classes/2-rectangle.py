cc2e9b32d3d5f568a27e48598f5936798836ff58
Sergioarg <sergio_ramos53@yahoo.com>
2021-05-20 21:30:17 -0500

    Fix validation equal to 0

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -19,7 +19,6 @@ class Rectangle:
         if (type(value) != int):
             raise TypeError("width must be an integer")
         elif (value < 0):
-            value = 0
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -34,14 +33,23 @@ class Rectangle:
         if (type(value) != int):
             raise TypeError("height must be an integer")
         elif (value < 0):
-            value = 0
             raise ValueError("height must be >= 0")
         self.__height = value
 
     """ Function return the area of rectagle """
     def area(self):
-        return (self.__width * self.__height)
+        width = self.__width
+        height = self.__height
+        if width == 0 or height == 0:
+            width = 0
+            height = 0
+        return (width *  height)
 
     """ Function return the perimeter of rectagle """
     def perimeter(self):
-        return (2 * (self.__width + self.__height))
+        width = self.__width
+        height = self.__height
+        if width == 0 or height == 0:
+            width = 0
+            height = 0
+        return (2 * (width + height))
