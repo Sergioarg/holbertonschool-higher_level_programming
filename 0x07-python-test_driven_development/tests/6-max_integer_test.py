cf42d288f0538d091b78452f533d8eca03cf1896
Sergioarg <sergio_ramos53@yahoo.com>
2021-05-26 18:37:02 -0500

    (Add) correct code

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,25 +1,42 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list
-"""
-
 
 import unittest
+max_integer = __import__('6-max_integer').max_integer
 
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+class TestMaxInteger(unittest.TestCase):
 
+    """ Function to make test normal integers """
 
-def test_max_integer(self):
-    print("Toy testeando")
+    def test_max_integer(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([10, 2, 30, 4]), 30)
+        self.assertEqual(max_integer([2, 3, 5, 1]), 5)
+
+    """ Function to make test float values """
+
+    def test_float(self):
+        self.assertEqual(max_integer([1.0, 2.0, 3.0, 4.0]), 4.0)
+        self.assertEqual(max_integer([10.0, 2.0, 60.0, 4.0]), 60.0)
+        self.assertEqual(max_integer([2.0, 3.0, 5.0, 1.0]), 5.0)
+
+    """ Function to make test negatives values """
+
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([-10, -2, -30, -4]), -2)
+        self.assertEqual(max_integer([-2, -3, -5, -1]), -1)
+
+    """ Function to make test float values """
+
+    def test_mixed(self):
+        self.assertEqual(max_integer([-1, 2.0, 40, -69]), 40)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer("Python"), 'y')
+
+    """ Function to get  raise message errors """
+
+    def test_raise_messages(self):
+        self.assertRaises(TypeError, max_integer, [98, '98'])
+        self.assertRaises(TypeError, max_integer, ['98', 25, -3, 2.6])
+        self.assertRaises(TypeError, max_integer, None)
