e3f39846e395876a93d3c6f06d1d9ca3b36f7c44
Sergioarg <sergio_ramos53@yahoo.com>
2021-05-25 22:01:09 -0500

    Add 0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module to add two integers"""
+
+
+def add_integer(a, b=98):
+    """Function to return the result of two integers
+    """
+    if not(type(a) == int or type(a) == float):
+        raise TypeError("a must be an integer")
+    elif not(type(b) == int or type(b) == float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
