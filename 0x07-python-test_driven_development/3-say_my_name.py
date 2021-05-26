82fd6fe97b13b22e6cf4a6466229d9b25af6e391
Sergioarg <sergio_ramos53@yahoo.com>
2021-05-25 22:04:40 -0500

    (Add) 3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module for print message """
+
+
+def say_my_name(first_name, last_name=""):
+    """Validation of strings to print"""
+    if (type(first_name) != str):
+        raise TypeError("first_name must be a string")
+    elif (type(last_name) != str):
+        raise TypeError("last_name must be a string")
+    print("My name is ", first_name, last_name, end="\n")
