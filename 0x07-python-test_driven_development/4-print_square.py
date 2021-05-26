e723b4284d68e6983775b2d4180f9f83bbfb9821
Sergioarg <sergio_ramos53@yahoo.com>
2021-05-25 22:09:26 -0500

    (Add) 4-print_square.py

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module to print an square """
+
+
+def print_square(size):
+    """ Validations of data type and loop to print the ractangle"""
+    if (type(size) != int):
+        raise TypeError("size must be an integer")
+    elif (size < 0):
+        raise ValueError("size must be >= 0")
+    elif (size < 0 and type(size) == float):
+        raise TypeError("size must be an integer")
+    else:
+        for row in range(size):
+            for col in range(size):
+                print("#", end="")
+            print("")
