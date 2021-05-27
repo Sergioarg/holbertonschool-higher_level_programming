0458c2fff5685169d0c30eea0f7e84d0bf4dc33f
Sergioarg <sergio_ramos53@yahoo.com>
2021-05-27 13:14:03 -0500

    (Refactor) conditional and add test cases

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -4,12 +4,10 @@
 
 def print_square(size):
     """ Validations of data type and loop to print the ractangle"""
-    if (type(size) != int):
+    if (type(size) != int or type(size) == float):
         raise TypeError("size must be an integer")
     elif (size < 0):
         raise ValueError("size must be >= 0")
-    elif (type(size) == float):
-        raise TypeError("size must be an integer")
     else:
         for row in range(size):
             for col in range(size):
