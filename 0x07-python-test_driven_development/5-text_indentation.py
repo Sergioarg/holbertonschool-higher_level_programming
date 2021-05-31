21cb8d4ac614ddff943c7c51be616ddc79f26807
Sergioarg <sergio_ramos53@yahoo.com>
2021-05-30 21:25:48 -0500

    (Fix) no spaces after the nes lines

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,6 +7,10 @@ def text_indentation(text):
 
     if (type(text) != str):
         raise TypeError("text must be a string")
-    else:
-        text_mod = text.translate({46: '.\n\n', 58: ':\n\n', 63: '?\n\n'})
-    print(text_mod, end="")
+    text_mod = text.translate({46: '.\n\n', 58: ':\n\n', 63: '?\n\n'})
+
+    text_mod = text_mod.splitlines()
+    for i, line in enumerate(text_mod):
+        if (i != 0):
+            print("")
+        print(line.strip(), end="")
