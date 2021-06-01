41132826573d0c12fe14d379f189e0c47767b344
Sergioarg <sergio_ramos53@yahoo.com>
2021-06-01 01:42:13 -0500

    (Add) 6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module to decode string and json """
+import json
+
+
+def load_from_json_file(filename):
+    """ Function that creates an Object from a “JSON file”
+
+        Args:
+            filename (str): File name used
+    """
+    with open(filename, encoding="utf-8") as my_file:
+       return json.load(my_file)
