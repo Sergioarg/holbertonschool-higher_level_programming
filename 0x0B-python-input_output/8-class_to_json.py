2fa21df045fbeefc5f65e989664b81747ded437d
Sergioarg <sergio_ramos53@yahoo.com>
2021-06-01 17:38:18 -0500

    (Doc) update documentation

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-""" Module that returns an dictianary"""
+""" Module that returns an dictionary
+    description with simple data structure.
+"""
 
 
 def class_to_json(obj):
     """ Function class_to_json
 
-        Args:
+    Args:
         obj (dict): instance of a Class
     """
     return obj.__dict__
