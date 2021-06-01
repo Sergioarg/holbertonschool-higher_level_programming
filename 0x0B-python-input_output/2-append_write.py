af57d27bfc7aa919cc7b9fe3ea228a0d6bc8b3ff
Sergioarg <sergio_ramos53@yahoo.com>
2021-05-31 23:43:12 -0500

    (Doc) Update documentation

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" Module to open and write files """
+""" Module to open and append files """
 
 
 def append_write(filename="", text=""):
-    """ Function to write the content of the file  """
+    """ Function to append the content of the file  """
 
     with open(filename, mode="a+", encoding="utf-8") as my_file:
         return my_file.write(text)
