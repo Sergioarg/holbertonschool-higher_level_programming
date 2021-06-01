a8ba87fd64876bef9c2d3168097620f61fc96953
Sergioarg <sergio_ramos53@yahoo.com>
2021-05-31 22:32:38 -0500

    (Add) 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module to open and read files """
+
+
+def read_file(filename=""):
+    """ Function to read the content of the file  """
+
+    with open(filename, mode="r",encoding="utf-8") as my_file:
+        print(my_file.read())
