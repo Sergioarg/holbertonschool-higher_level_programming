6cf69c3c7e418ebf73f837cd8502d6118a46d308
Sergioarg <sergio_ramos53@yahoo.com>
2021-06-02 18:08:34 -0500

    (Doc) add documentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+""" Module that returns the list """
+
+
 def lookup(obj):
+    """ Function that returns the list
+
+    Args:
+        obj (list): object listg
+    """
     return list(obj.__dict__)
