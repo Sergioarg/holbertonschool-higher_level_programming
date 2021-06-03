444344c46eec0c4d7ab0a90e8a02f15afb8d13d4
Sergioarg <sergio_ramos53@yahoo.com>
2021-06-02 23:36:01 -0500

    (Add) 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Module of class Square """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+
+    """ Function contructor that instance the private attributes
+
+        Atrributes:
+            size (int): size
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return ("[Square] {:d}/{:d}".format(self.__size, self.__size))
