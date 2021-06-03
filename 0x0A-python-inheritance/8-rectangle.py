4ce167b7bd555e18f4b2836ef99d42fc3fca91a8
Sergioarg <sergio_ramos53@yahoo.com>
2021-06-02 23:10:49 -0500

    (Add) 8-rectangle.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Module of class BaseGeometry """
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Function contructor that instance the private attributes
+
+        Atrributes:
+            width (int): width
+            height (int): height
+    """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
