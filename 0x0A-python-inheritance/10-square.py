45cb053ce2625d555149492a97028f707af6b692
Sergioarg <sergio_ramos53@yahoo.com>
2021-06-02 23:29:28 -0500

    (Add) 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module of class Square """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+
+    """ Function contructor that instance the private attributes
+
+        Atrributes:
+            size (int): size
+    """
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
