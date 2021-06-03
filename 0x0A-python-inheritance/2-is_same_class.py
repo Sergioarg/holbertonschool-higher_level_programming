3f39df8488d98daaec20eb3895bbb168cd4d8b70
Sergioarg <sergio_ramos53@yahoo.com>
2021-06-02 22:31:49 -0500

    (Add) 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module to validate if obj is the same type of a_class """
+
+
+def is_same_class(obj, a_class):
+    """ Function that validate if the obj is the same type """
+
+    if type(obj) == a_class:
+        True
+    else:
+        False
