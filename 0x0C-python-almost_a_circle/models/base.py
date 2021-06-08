a393ee096d40d39d1c74a3efcafe5be63bc2027a
Sergioarg <sergio_ramos53@yahoo.com>
2021-06-08 16:13:53 -0500

    (Add) validation that if list is empty

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,7 +21,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """ Returns the JSON string representation of list_dictionaries """
-        if list_dictionaries is None:
+        if len(list_dictionaries) == 0 or list_dictionaries is None:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
