609da18cca5161cf5c26c4819f778117aae36169
Sergioarg <sergio_ramos53@yahoo.com>
2021-06-08 16:10:21 -0500

    (Add) public method to_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Module to class Base """
+import json
 
 
 class Base:
@@ -7,8 +8,20 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ Class constructor:
+
+        Args:
+            id (int): id of constructor
+        """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ Returns the JSON string representation of list_dictionaries """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
