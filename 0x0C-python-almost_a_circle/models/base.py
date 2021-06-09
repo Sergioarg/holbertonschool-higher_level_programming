89cdbec73d11a4c1a555a16e5ee135e2c5ccc9e6
Sergioarg <sergio_ramos53@yahoo.com>
2021-06-08 23:32:22 -0500

    Update model base correct function

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -41,7 +41,7 @@ class Base:
         if json_string is None or json_string == '':
             return []
         else:
-            return json.load(json_string)
+            return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
