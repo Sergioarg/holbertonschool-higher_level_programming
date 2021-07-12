ce27d399278404c24b7032ece777e8940a815430
Sergioarg <sergio_ramos53@yahoo.com>
2021-07-12 17:50:24 -0500

    :tada: Create files with basics SQL scripts

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- creates the database hbtn_0c_0 in your MySQL server.
+CREATE DATABASE [IF NO EXISTS] hbtn_0c_0;
+CREATE TABLE (first_table (id INT), name VARCHAR(256));
---
+++ 0x0D-SQL_introduction/README.md
@@ -0,0 +1,4 @@
+# SQL Introduction
+
+In this folder you can find diferents commands to use in MySQL
+
