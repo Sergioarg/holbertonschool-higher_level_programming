340f7dd8df57f47f5231a4733c616e7d7e02b808
Sergioarg <sergio_ramos53@yahoo.com>
2021-07-12 18:21:55 -0500

    :bug: Fix SQL's script that creates a table in BD

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,2 @@
 -- creates the database hbtn_0c_0 in your MySQL server.
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-CREATE TABLE (first_table (id INT), name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
