da070cc4afcf1fba597de16747a221aa2379d6d5
Sergioarg <sergio_ramos53@yahoo.com>
2021-07-12 17:54:51 -0500

    :bug: Fix SQL's scipts of validations

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,3 @@
 -- creates the database hbtn_0c_0 in your MySQL server.
-CREATE DATABASE [IF NO EXISTS] hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 CREATE TABLE (first_table (id INT), name VARCHAR(256));
