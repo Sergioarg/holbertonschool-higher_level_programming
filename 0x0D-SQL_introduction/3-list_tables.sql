b089a852b5179f4e1fc84536c03435bbf3f77a23
Sergioarg <sergio_ramos53@yahoo.com>
2021-07-12 18:04:35 -0500

    :bug: Fix SQL's script that show tables

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,2 @@
--- creates the database hbtn_0c_0 in your MySQL server.
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-CREATE TABLE (first_table (id INT), name VARCHAR(256));
+-- Lists all the tables of a database in your MySQL server.
+SHOW TABLES;
