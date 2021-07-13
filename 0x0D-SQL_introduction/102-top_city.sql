98c37af7c01d564a09cf1501ecb9df99f749a106
Sergioarg <sergio_ramos53@yahoo.com>
2021-07-12 23:43:23 -0500

    :tada: Create SQl script to get the avarage of temperatures with limit of 3

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- Average temperature (Fahrenheit) by city ordered by temperature (descending).
+SELECT city, AVG(value) avg_temp
+FROM temperatures
+WHERE month BETWEEN 7 AND 8
+GROUP BY city
+ORDER BY AVG(value) DESC
+LIMIT 3;
