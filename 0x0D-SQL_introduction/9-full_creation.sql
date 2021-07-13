aafd749914517e9f6ad1b46acfdb99c0e9da9f6d
Sergioarg <sergio_ramos53@yahoo.com>
2021-07-12 20:05:02 -0500

    :wrench: Fix SQL script to insert values in the new table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,6 +2,16 @@
 -- and add multiples rows.
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
-    nama VARCHAR(256),
+    name VARCHAR(256),
     score INT
 );
+
+INSERT INTO second_table (id, name, score)
+VALUES
+    (1, "Jhon", 10 ),
+
+    (2, "Alex", 3),
+
+    (3, "Bob", 14),
+
+    (4, "George", 8);
