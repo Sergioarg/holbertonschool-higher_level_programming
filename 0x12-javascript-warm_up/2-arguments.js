97031c9c2cfb1be6625099be148bf57ed6fc48e7
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-02 18:39:58 -0500

    test

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const length = process.argv.slice(2).length;
 if (length === 0) {
   console.log('No argument');
-} else if (process.argv.length === 1) {
+} else if (length === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
