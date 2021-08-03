c2c17cbc9312ab5890640540856509480221d5c8
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-03 18:55:41 -0500

    :tada: Create initial files with condional

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+const length = process.argv.slice(2).length;
+// const number = parseInt(Number(process.argv[2]));
+if (length === 0 || length === 1) {
+  console.log('0');
+} else {
+  console.log('Arguments found');
+}
