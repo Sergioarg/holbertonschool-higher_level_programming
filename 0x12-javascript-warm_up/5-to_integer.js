d03d1240b6b35289d1ecf45fdb2933bd0b417b49
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-02 22:36:39 -0500

    :bug: Change mode to print the messages

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const number = parseInt(Number(process.argv[2]));
 if (isNaN(number)) {
   console.log('Not a number');
 } else {
-  console.log(`My nomber: ${number}`);
+  console.log(`My number: ${number}`);
 }
