fc236e5d2aa705ca863de3cec8ec5fffebd9cffc
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-03 23:54:02 -0500

    Add function that counts the ocurrence

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (const value in list) {
+    if (list[value] === searchElement) {
+      count++;
+    }
+  }
+  return (count);
+};
