d98a19b830f579133fd94c2de4e38ea696b17bd8
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-03 23:39:33 -0500

    :wrench:Add code with new class of the original square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const parentSquare = require('./5-square');
+
+class Square extends parentSquare {
+  charPrint (c) {
+    if (!c) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.with; i++) {
+        console.log(c.repeat(this.with));
+      }
+    }
+  }
+}
+
+module.exports = Square;
