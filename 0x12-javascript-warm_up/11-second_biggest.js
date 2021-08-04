4832c48a7b0617a9f5583802ed481777ef412882
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-04 10:11:55 -0500

    fix to correct structure of code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
+const value = process.argv.slice(2)
+  .sort(function (a, b) {return b - a; });
 
-const length = process.argv.slice(2).length;
-// const number = parseInt(Number(process.argv[2]));
-if (length === 0 || length === 1) {
-  console.log('0');
+if (value > 1) {
+  console.log(value[1]);
 } else {
-  console.log('Arguments found');
+  console.log(0);
 }
