2dd8198bcb3b0b8dfe2b9ad96286b69de569f420
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-07 17:28:18 -0500

    :tada: Update files and add docker

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    root = sys.argv[1]
+    password = sys.argv[2]
+    data_base = sys.argv[3]
+    searched = sys.argv[4]
+
+    # Open database connection
+    # db = MySQLdb.connect(host="localhost", user=root,passwd=password,
+    # db=data_base, port=3306)
+    db = MySQLdb.connect(host="localhost", user=root, db=data_base, port=3306)
+
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # execute SQL query using execute() method.
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    # Fetch a single row using fetchall() method.
+    data = cursor.fetchall()
+
+    for element in data:
+        print(element)
+    # disconnect from server
+    db.close()
+    cursor.close()
