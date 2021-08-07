534da58b747636fefeb3b9ffabc345ae52d19c2b
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-07 17:19:48 -0500

    :sparkles: Add code with specific qery

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
+""" Module to connect to the database and print the data from the
+    Table: States with specific number.
+"""
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
+
     root = sys.argv[1]
     password = sys.argv[2]
-    data_base = sys.argv[3]
+    db_name = sys.argv[3]
     searched = sys.argv[4]
 
     # Open database connection
-    # db = MySQLdb.connect(host="localhost", user=root,passwd=password,
-    # db=data_base, port=3306)
-    db = MySQLdb.connect(host="localhost", user=root, db=data_base, port=3306)
+    db = MySQLdb.connect(host="localhost", port=3306, user=root,
+                         passwd=password, db=db_name)
 
     # prepare a cursor object using cursor() method
     cursor = db.cursor()
 
     # execute SQL query using execute() method.
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cursor.execute(
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(searched))
 
     # Fetch a single row using fetchall() method.
     data = cursor.fetchall()
 
-    for element in data:
-        print(element)
+    for state in data:
+        print(state)
+
     # disconnect from server
     db.close()
     cursor.close()
