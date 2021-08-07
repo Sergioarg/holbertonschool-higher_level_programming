5be7ce2287c48bc68f673c810c6626032274ae70
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-07 16:12:15 -0500

    :wrench: Update correct documentation and passwd to connect

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
+""" Module to connect to the database and print the data from the
+    Table: States .
+"""
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
+
     root = sys.argv[1]
     password = sys.argv[2]
-    data_base = sys.argv[3]
+    db_name = sys.argv[3]
 
     # Open database connection
-    # db = MySQLdb.connect(host="localhost", user=root,passwd=password,
-    # db=data_base, port=3306)
-    db = MySQLdb.connect(host="localhost", user=root, db=data_base, port=3306)
+    db = MySQLdb.connect(host="localhost", port=3306, user=root,
+                         passwd=password, db=db_name)
 
     # prepare a cursor object using cursor() method
     cursor = db.cursor()
@@ -23,6 +27,7 @@ if __name__ == "__main__":
 
     for element in data:
         print(element)
+
     # disconnect from server
     db.close()
     cursor.close()
