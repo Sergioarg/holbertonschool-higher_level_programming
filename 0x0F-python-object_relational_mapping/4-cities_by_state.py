76588da4636238dbf3e4c2f72038e73f40d588f2
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-08 13:47:13 -0500

    :bug: Fix query with correct ON staments

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,20 +6,19 @@
 import MySQLdb
 import sys
 
-from sqlalchemy.sql.functions import user
-
 if __name__ == "__main__":
 
-    user, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    usr, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=user,
+    db = MySQLdb.connect(host="localhost", port=3306, user=usr,
                          passwd=password, db=db_name)
 
     cursor = db.cursor()
 
     cursor.execute("""SELECT cities.id, cities.name, states.name
                     FROM cities
-                    INNER JOIN states ON cities.states_id = states.id
+                    INNER JOIN states
+                    ON cities.state_id = states.id
                     ORDER BY cities.id ASC""")
 
     data = cursor.fetchall()
