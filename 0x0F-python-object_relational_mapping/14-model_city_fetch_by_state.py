0bba6c3d4aa790c21a819579606e1849a0c570ae
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-08 13:59:19 -0500

    :bug: Fix correct messages of format

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
                           ).join(City, State.id == City.state_id)
 
     for state_city in query.order_by(City.id):
-        print("{:s}: {:d} {:s}".format(
+        print("{:s}: ({:d}) {:s}".format(
             state_city.state__name, state_city.city_id, state_city.city_name))
 
     session.close()
