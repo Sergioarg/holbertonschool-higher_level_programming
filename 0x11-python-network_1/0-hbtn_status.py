31eb226a9ca0c4a74693a672bb83c2ccc368ed65
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-20 14:52:19 -0500

    :tada: Create folder and file number 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module to Python script that fetches https://intranet.hbtn.io/status """
+import urllib.request
+
+
+if __name__ == "__main__":
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+
+    print("Body response:$")
+    print("    - type: {}".format(type(html)))
+    print("    - content: {}".format(html))
+    print("    - utf8 content: {}".format(html.decode('UTF-8')))
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,3 @@
+# 0x11-python-network_1
+
+_We will manage Python and Network._
