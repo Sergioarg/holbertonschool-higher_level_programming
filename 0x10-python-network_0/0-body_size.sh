5eab2ffc9c25e47862474c4ec33cf69451e3d2a6
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-19 20:47:20 -0500

    Delete usage of variable

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Displays the size of the body of the response.
-URL=$1
-curl -sI $URL | grep "Content-Length" | awk '{print $2}'
+curl -sI $1 | grep "Content-Length" | awk '{print $2}'
