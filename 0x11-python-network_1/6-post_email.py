c7264d99d60b8d38696db184934fb543b35f6fc0
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-20 20:45:02 -0500

    Add code to file 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""  sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8) """
+
+import urllib.request
+import urllib.parse
+from urllib import parse
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    url = argv[1]
+    email = argv[2]
+
+    data = {"email": email}
+    req = requests.post(url, data=data)
+    print(req.text)
