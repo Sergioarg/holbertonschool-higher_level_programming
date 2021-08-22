095350630aae26dc7f7f727256c919746a3cc3f9
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-21 21:38:14 -0500

    :tada: Add code for the file 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+""" Python script that takes in a URL, sends a request to the URL and
+displays the body of the response. """
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+
+    url = "http://0.0.0.0:5000/search_user"
+
+    # First get the page
+    if len(argv) > 1:
+        query = argv[1]
+    data = {'q': query}
+
+    req = requests.post(url=url, data=data)
+
+    try:
+        # Request to JSON file
+        req_json = req.json()
+
+        if len(req_json) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(req_json.get('id'), req_json.get('name')))
+
+    except:
+        print("Not a valid JSON")
