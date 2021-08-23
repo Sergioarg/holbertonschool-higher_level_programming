1307f3ff7521f0ab724aaeab49f3c9c555ce7c8d
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-23 01:08:26 -0500

    :tada: Create python files

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+""" Module for find the peak """
+
+
+def find_peak(list_of_integers):
+    """ Function that fintd the peak """
+    if not list_of_integers:
+        return None
+    else:
+        list_of_integers.sort()
+        return(list_of_integers[-1])
