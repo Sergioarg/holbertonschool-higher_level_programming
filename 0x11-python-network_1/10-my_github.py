b335aec8f0e18457ce04548f62958a42a9bfd44c
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-23 01:21:57 -0500

    Create code for taks 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+import sys
+import requests
+from sys import argv
+
+from requests.models import to_key_val_list
+
+if __name__ == "__main__":
+
+    username = argv[1]
+    password = argv[2]
+    github = "https://api.github.com/user"
+
+    req = requests.get(url=github, auth=(username, password))
+    print(req.json().get("id"))
