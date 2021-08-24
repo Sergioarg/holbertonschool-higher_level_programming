0c4d0bd33852ffe17f51135c9ed4b97004d0bdee
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-24 10:11:46 -0500

    :tada: Create file 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Module for use the GitHub API """
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    repo = argv[1]
+    user = argv[2]
+
+    github = "https://api.github.com/repos/{}/{}/commits".format(repo, user)
+
+    req = requests.get(github)
+
+    for commit in req.json():
+        sha = commit.get("sha")
+        user = commit.get("commit").get("author").get("name")
+        print("{}: {}".format(sha, user))
