49ffd68667014ed59e1cb64cccd74a71f1664cd0
Sergioarg <sergio_ramos53@yahoo.com>
2021-09-23 03:43:30 +0000

    :heavy_plus_sign: Update files with js code

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,6 +1,1 @@
-<head>
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-</head>
-
-// document.querySelector("header").style.color = "#FF0000";
-document.querySelector("h2, h3").style.backgroundColor = "red";
+document.querySelector('header').style.color = '#FF0000';
